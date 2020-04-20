20ab19e78ff637a6448f62057112bd626c7cfb9c
William Gurecky <william.gurecky@gmail.com>
2020-04-20 04:53:59 -0500

    adding additional unit test for vis_math command

--- test/test_nvim.py
+++ test/test_nvim.py
@@ -5,24 +5,54 @@ import os
 sys.path.append("../rplugin/python3/.")
 from visSum import VimSumPlug
 
-# create nvim instance
-nvim = attach('child', argv=["env", "nvim", "--embed", "--headless"])
+def vis_mult_test():
+    # create nvim instance
+    nvim = attach('child', argv=["env", "nvim", "--embed", "--headless"])
 
-# load the input file
-nvim.command("edit VisMult.inp.txt")
+    # load the input file
+    nvim.command("edit VisMult.inp.txt")
 
-# Initilize vimsum plugin
-vsp = VimSumPlug(nvim)
+    # Initilize vimsum plugin
+    vsp = VimSumPlug(nvim)
 
-# set visual mode marks
-nvim.feedkeys("m<")
-nvim.feedkeys("$")
-nvim.feedkeys("m>")
+    # set visual mode marks
+    nvim.feedkeys("m<")
+    nvim.feedkeys("$")
+    nvim.feedkeys("m>")
 
-# Execute VisMult
-args = ['2.0', '5e']
-range = [1, 1]
-vsp.vis_mult(args, range)
+    # Execute VisMult
+    args = ['2.0', '5e']
+    range = [1, 1]
+    vsp.vis_mult(args, range)
 
-# Write resulting file out
-nvim.command("wq! VisMult.out.txt")
+    # Write resulting file out
+    nvim.command("wq! VisMult.out.txt")
+    nvim.close()
+
+def vis_math_test():
+    # create nvim instance
+    nvim = attach('child', argv=["env", "nvim", "--embed", "--headless"])
+
+    # load the input file
+    nvim.command("edit VisMult.inp.txt")
+
+    # Initilize vimsum plugin
+    vsp = VimSumPlug(nvim)
+
+    # set visual mode marks
+    nvim.feedkeys("m<")
+    nvim.feedkeys("$")
+    nvim.feedkeys("m>")
+
+    # Execute VisMult
+    args = ['2.0*(x)', '5e']
+    range = [1, 1]
+    vsp.vis_math(args, range)
+
+    # Write resulting file out
+    nvim.command("wq! VisMath.out.txt")
+    nvim.close()
+
+if __name__ == "__main__":
+    vis_mult_test()
+    vis_math_test()
--- test/test_nvim.sh
+++ test/test_nvim.sh
@@ -3,3 +3,4 @@
 # test VisMult
 python test_nvim.py
 diff VisMult.gold.txt VisMult.out.txt
+diff VisMult.gold.txt VisMath.out.txt
